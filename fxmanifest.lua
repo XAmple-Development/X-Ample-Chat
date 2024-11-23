816ca62d5a8c9776aa8bb8c66c45c36fdef6f1a8
Danny <chasebreton.cbreton@gmail.com>
2024-11-23 21:14:40 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+
+game        "common"
+
+author      "X-Ample Development"
+
+dependency "chat"
+
+chat_theme "chat-design-v1" {
+    styleSheet = "chat-design-v1.css"
+}
+
+files {
+    "chat-design-v1.css",
+}
